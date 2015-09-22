3d9b030b4944320e237d0fcc8ca79f814325ef68
lefthook123 <sf.jackwang@gmail.com>
2015-09-22 16:33:44 -0700

    Office 9.22

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('AccelApp',["ui.router",'ngAnimate']);
+var app = angular.module('WPApp',["ui.router",'ngAnimate']);
 app.config(function($stateProvider, $urlRouterProvider,$locationProvider){
 	$urlRouterProvider.otherwise('/home');
 	$stateProvider
@@ -80,22 +80,15 @@ app.controller('mainController',function($scope){
 });
 app.controller('teamController',function($scope){
 	$scope.pageClass = 'page-team';
-    $scope.toddimgPath='images/todd.jpg';
-    $scope.murphyimgPath='images/murphy.jpg';
 
     $scope.managers= [
        
-        {name:'Jane Murphy',
-        alt:'Jane Murphy',
-        path:'images/murphy.jpg',
+        {name:'Jack Wang',
+        alt:'Jack Wang',
+        path:'images/JackWang2.jpg',
         title:'Founder',
-        description:'Jane has been selling financial services since the 1980\'s and focused on the retirement plan business since the early 1990\'s. Working for Fidelity Investments, she built a sales organization from 3 employees to well over 100. She also built a lead generation team from scratch to over 60 associates. Jane is well known for highly innovative sales approaches and processes. Her team produced over $3 billion in assets under management per year while selling no plan with greater than $5 million in assets. Jane\'s lead generation team produced over 3,000 leads a year in all sizes of the retirement plan market including the tax exempt market. Jane is a sought after leader well known for turning lead generation associates into salespeople.'}
-        ,
-        {name:'Todd Jenkins',
-        alt:'Todd Jenkins',
-        path:'images/todd.jpg',
-        title:'Vice President, New Business Development ',
-        description:'Todd Jenkins has extensive financial services experience working with insurance and retirement plan sales.  In his second year at Fidelity Investments, Todd was able to generate over $70M in new retirement plan assets while working in the less than $5M market. He used the same techniques that Acceleration Retirement provides to Advisors. His deep understanding of Advisors and sales make him an ideal strategic partner for progressive Advisors.'}
+        description:'Technology evangelist on \'Salesforce CRM\', \'Web Development\''}
+      
     ];
 
 });

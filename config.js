372969f28bd0ca70d0ba1014ab05a7aab3471df0
lefthook123 <zxw120230@utdallas.edu>
2015-10-09 22:53:15 -0700

    10.9 from home
    change mongodb path

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     'secret': 'lonestar1',
-    'database': 'mongodb://127.0.0.1:27017/WPblog'
+    //'database': 'mongodb://127.0.0.1:27017/WPblog'
+    'database': 'mongodb://zxw120230:password@ds051843.mongolab.com:51843/heroku_tgzjkx0j'
 };

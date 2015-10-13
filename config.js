ecd33db81182bf57d93cb2e0d7afb5f1c2c9cc8a
lefthook123 <sf.jackwang@gmail.com>
2015-10-13 14:17:27 -0700

    10.13 from office

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'lonestar1',
-    'database': 'mongodb://127.0.0.1:27017/WPblog'
-    //'database': 'mongodb://zxw120230:admin@ds051843.mongolab.com:51843/heroku_tgzjkx0j'
+    //'database': 'mongodb://127.0.0.1:27017/WPblog'
+    'database': 'mongodb://zxw120230:admin@ds051843.mongolab.com:51843/heroku_tgzjkx0j'
 };
